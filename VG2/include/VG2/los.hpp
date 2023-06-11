9de6ad2d77d05c7e7bc7a4c85f4704744e3c7ac0
Lai Yan Kai <lai.yankai@gmail.com>
2023-06-12 01:40:46 +0800

    230612

--- VG2/include/VG2/los.hpp
+++ VG2/include/VG2/los.hpp
@@ -11,11 +11,7 @@ namespace P2D::VG2
     class Los
     {
     private:
-        V2 pi, pf, dir, abs_dir, sgn_dir;
         Grid *const grid;
-        mapkey_t ki, kf;
-        dir_idx_t di;
-        char dim_long;
 
     public:
         Los(Grid *const &grid, std::filesystem::path &fp_vg) : grid(grid) {}
@@ -23,129 +19,112 @@ namespace P2D::VG2
         Los(const Los &) = delete;
         ~Los() {}
 
-        // Tests visibility between start vertex with key ki, coordinate vi, and destination vertex with key kf, coordinate vf.
+        // Tests visibility between start vertex with key ki, coordinate vi, and destination vertex with key kf and coordinate vf.
         // Returns true if visible.
         // Assumes the grid is rectangle, and both vertices are in map.
+        template <bool diag_block_at_start = false>
         bool cast(const mapkey_t &ki, const V2 &pi, const mapkey_t &kf, const V2 &pf) const
         {
-            assert(grid->inMap<false>(vi) == true);
-            assert(grid->inMap<false>(vf) == true);
+            assert(grid->inMap<false>(pi) == true);
+            assert(grid->inMap<false>(pf) == true);
             assert(ki == grid->coordToKey<false>(pi));
             assert(kf == grid->coordToKey<false>(pf));
 
             if (ki == kf) // nothing to check
                 return true;
 
-            this->ki = ki;
-            this->kf = kf;
-            this->pi = pi;
-            this->pf = pf;
-            dir = pf - pi;
-            abs_dir = P2D::abs(dir);
-            sgn_dir = P2D::sgn(dir);
-            di = dirToDirIdx(dir);
-            dim_long = abs_dir.x >= abs_dir.y ? 0 : 1;
+            V2 dir = pf - pi;
+            V2 abs_dir = P2D::abs(dir);
+            V2 sgn_dir = P2D::sgn(dir);
+            dir_idx_t di = dirToDirIdx(dir);
+            bool dim_long = abs_dir.x >= abs_dir.y ? 0 : 1;
 
             if (isCardinal(di) == true)
-                _castCardinal();
+                return _castCardinal<diag_block_at_start>(pi, di, dim_long, abs_dir);
             else
-                _castOrdinal();
+                return _castOrdinal<diag_block_at_start>(ki, pi, di, dim_long, abs_dir, sgn_dir);
         }
 
     private:
-        bool _castOrdinal() const
+        template <bool diag_block_at_start>
+        bool _castOrdinal(mapkey_t kv, const V2 &pi, const dir_idx_t &di, const bool &dim_long, const V2 &abs_dir, const V2 &sgn_dir) const
         {
-            const dir_idx_t fwd_dir_idx = dirToDirIdx<V2>(sgn_diff);
-            const mapkey_t &rcfv_f_key = grid->getAdjRelKeyCFV(fwd_dir_idx);
-            const dir_idx_t l_dir_idx = addDirIdx<true>(fwd_dir_idx, 2);
-            [[maybe_unused]] const mapkey_t &rcfv_l_key = grid->getAdjRelKeyCFV(l_dir_idx); // for blocking implementations only
-            const dir_idx_t r_dir_idx = addDirIdx<true>(fwd_dir_idx, 6);
-            [[maybe_unused]] const mapkey_t &rcfv_r_key = grid->getAdjRelKeyCFV(r_dir_idx); // for blocking implementations only
+            assert(sgn_dir.x != 0 && sgn_dir.y != 0);
+            bool is_rh_frame = (dim_long == 0) == (sgn_dir.x == sgn_dir.y); // provided that sgn_dir for both not zero (i.e. di is ordinal)
+            dir_idx_t di_long = addDirIdx(di, is_rh_frame ? 7 : 1);
+            dir_idx_t di_short = addDirIdx(di, is_rh_frame ? 1 : 7);
 
-            V2 coord = sgn_diff;
-            coord[longer_dim] = 0;
-            const dir_idx_t shorter_dir_idx = dirToDirIdx<V2>(coord);
-            const mapkey_t &rel_short_key = grid->getAdjRelKey(shorter_dir_idx); // rel short direction
+            mapkey_t rkv_long = grid->getRelKey<false>(di_long);
+            mapkey_t rkv_short = grid->getRelKey<false>(di_short);
 
-            coord[longer_dim] = sgn_diff[longer_dim];
-            coord[shorter_dim] = 0;
-            const dir_idx_t longer_dir_idx = dirToDirIdx<V2>(coord);
-            const mapkey_t &rel_long_key = grid->getAdjRelKey(longer_dir_idx); // rel long direction
+            mapkey_t kc = grid->addKeyToRelKey(kv, grid->getCellRelKey(di, pi.x)); // currently the cell in di direction (front)
+            mapkey_t rkc_long = grid->getRelKey<true>(di_long);
+            mapkey_t rkc_short = grid->getRelKey<true>(di_short);
 
-            // init at begin
-            coord = begin;
-            mapkey_t key = grid->coordToKey<V2>(begin);
+            // ======= check diag_block_at_start =========
+            if constexpr (diag_block && diag_block_at_start)
+            {                                                                                           // check diag_block at start (need to check oom for this stage)
+                V2 coord = grid->keyToCoord<true>(kc) + grid->getRelCoord<true>(addDirIdx(di_long, 4)); // coord in reverse di_long direction
+                mapkey_t key = grid->addKeyToRelKey(kc, -rkc_long);
+                bool is_blocked_long = !grid->isAccessible(key, coord);
 
-            // init short axis
-            long_t err_s = 0;
-            const long_t shorter_abs_diff = abs_diff[shorter_dim];
-            const long_t longer_abs_diff = abs_diff[longer_dim];
+                coord = grid->keyToCoord<true>(kc) + grid->getRelCoord<true>(addDirIdx(di_short, 4)); // coord in reverse di_long direction
+                key = grid->addKeyToRelKey(kc, -rkc_short);
+                bool is_blocked_short = !grid->isAccessible(key, coord);
 
-            // track longer dim
-            int_t &longer_val = coord[longer_dim];
-            const int_t &longer_end = end[longer_dim];
-            const int_t &longer_sgn = sgn_diff[longer_dim];
+                if (is_blocked_long && is_blocked_short)
+                    return false; // blocked
+            }
 
-            mapkey_t fcell_key = grid->addKeyToRelKey(key, rcfv_f_key);
-            bool fcell_is_oc = grid->isOc(fcell_key);
-            while (1)
+            int_t step = 0;
+            int_t err_short = 0;
+            const int_t &abs_long = abs_dir[dim_long];
+            assert(abs_long != 0);
+            const int_t &abs_short = abs_dir[!dim_long];
+            while (true)
             {
-                if (fcell_is_oc)
-                    return collided;
+                // ====== check cell in front =====
+                if (grid->isOc(kc))
+                    return false; // front blocked
 
-                // find change in short axis
-                err_s += shorter_abs_diff;
-                if (err_s == longer_abs_diff)
-                { // intersects grid vertex
-                    // move to short
-                    key = grid->addKeyToRelKey(key, rel_short_key);
-
-                    // (5) reset error
-                    err_s = 0; // err_s -= longer_abs_diff
-                }
-                else if (err_s > longer_abs_diff)
+                // ====== check long short error =====
+                err_short += abs_short; // short_abs_dir
+                if (err_short >= abs_long)
                 { // intersects grid edge (parallel to long axis)
-                    // (1) move to vertex in short direction
-                    key = grid->addKeyToRelKey(key, rel_short_key);
-                    // (2) check cell in forward (long and short) direction
-                    fcell_key = grid->addKeyToRelKey(key, rcfv_f_key);
-                    if (grid->isOc(fcell_key))
-                        return collided;
+                    // (1) move in short direction
+                    kv = grid->addKeyToRelKey(kv, rkv_short);
+                    kc = grid->addKeyToRelKey(kc, rkc_short);
+
+                    // (2) check cell in front if did not intersect grid vertex
+                    if (err_short > abs_long && grid->isOc(kc))
+                        return false; // collided
+
                     // (3) reset error
-                    err_s -= longer_abs_diff;
+                    err_short -= abs_long;
                 }
-                // move to vertex in long direction
-                key = grid->addKeyToRelKey(key, rel_long_key);
-                longer_val += longer_sgn;
+
+                // move in long direction
+                kv = grid->addKeyToRelKey(kv, rkv_long);
+                kc = grid->addKeyToRelKey(kc, rkc_long);
 
                 // break if reached
-                if (longer_val == longer_end)
-                    break;
+                if (++step == abs_long)
+                    return true;
 
-                fcell_key = grid->addKeyToRelKey(key, rcfv_f_key);
-                fcell_is_oc = grid->isOc(fcell_key);
-
-                if constexpr (is_blocking)
+                // ====== Check Diagonal block =====
+                if (diag_block && err_short == 0)
                 {
-                    // evaluate if blocked
-                    if (err_s == 0 && !fcell_is_oc)
-                    { // forward is free and path intersects vertex
-                        // placed here to avoid evaluating at begin's coordinate ==> the cell behind the vertex is free to reach here
-                        mapkey_t lcell_key = grid->addKeyToRelKey(key, rcfv_l_key);
-                        mapkey_t rcell_key = grid->addKeyToRelKey(key, rcfv_r_key);
-                        bool lcell_is_oc = grid->isOc(lcell_key);
-                        bool rcell_is_oc = grid->isOc(rcell_key);
-
-                        if (lcell_is_oc && rcell_is_oc)
-                            return collided;
-                    }
+                    // Checked cells are always in map
+                    bool is_blocked_long = grid->isOc(grid->addKeyToRelKey(kc, -rkc_long));   // cell in reverse long dir
+                    bool is_blocked_short = grid->isOc(grid->addKeyToRelKey(kc, -rkc_short)); // cell in reverse short dir
+                    if (is_blocked_long && is_blocked_short)
+                        return false; // blocked
                 }
             }
-            return reached;
         }
 
-
-        bool _castCardinal() const
+        template <bool diag_block_at_start>
+        bool _castCardinal(const V2 &pi, const dir_idx_t &di, const bool &dim_long, const V2 &abs_dir) const
         {
             // get bl and br
             mapkey_t kc_l = grid->addKeyToRelKey(ki, grid->getCellRelKey(addDirIdx(di, 3), pi.x));
@@ -156,8 +135,13 @@ namespace P2D::VG2
             const bool out_of_map_right = grid->inMap<false>(pi + grid->getRelCoord(addDirIdx(di, 6)));
             const bool out_of_map_back = grid->inMap<false>(pi + grid->getRelCoord(addDirIdx(di, 4)));
 
-            bool is_blocked_left = out_of_map_left || out_of_map_back || grid->isOc(kc_l);
-            bool is_blocked_right = out_of_map_right || out_of_map_back || grid->isOc(kc_r);
+            bool is_blocked_left = false;
+            bool is_blocked_right = false;
+            if constexpr (diag_block = true && diag_block_at_start == false)
+            {
+                is_blocked_left = out_of_map_left || out_of_map_back || grid->isOc(kc_l);
+                is_blocked_right = out_of_map_right || out_of_map_back || grid->isOc(kc_r);
+            }
             char window = (is_blocked_left << 1) | (is_blocked_right);
 
             const int_t &max_step = abs_dir[dim_long];

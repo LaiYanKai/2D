ce568e99f3816d58f6a2f62bfe58bdad5c363347
Lai Yan Kai <lai.yankai@gmail.com>
2023-06-14 15:24:46 +0800

    230614

--- ANYA2/include/ANYA2/ANYA2.hpp
+++ ANYA2/include/ANYA2/ANYA2.hpp
@@ -18,6 +18,7 @@ namespace P2D::ANYA2
             _dbgtitle("[Cone] Expand Cone Node { " << node << " }");
             _dbginc;
 
+            // ====================== Add Turning Points on both (+y and -y) sides ======================
             for (const int_t dy : {-1, 1})
             {
                 const V2 &vec_y = dy == -1 ? node->v_min : node->v_max;
@@ -61,6 +62,57 @@ namespace P2D::ANYA2
                         }     // -dx, +dy cell is occupied
                     }         // +dx +dy cell is free
                 }             // has turning point if +dx is same as vec_y.x
+            }
+
+            // ====================== Scan cells ======================
+            mapkey_t kc_min;
+            V2 pc_min;
+            _dbg11("[Cone:-Y] -y Ray is (" << node->v_min << "). Root(" << node->crn->coord << ")");
+            if (node->dx == node->v_min.x)
+            { // ray cuts across min-y vertex on expanded interval (cur row)
+                if (node->v_min.y < 0)
+                { // kc_min depends on next row
+                    int_t dx_next = node->dx + (node->dx < 0 ? -1 : 1);
+                    const V2 vert_coord = node->crn->coord + V2{dx_next, dx_next * node->v_min.y / node->v_min.x};
+                    const mapkey_t vert_key = grid->coordToKey<false>(vert_coord);
+                    // kc_min is in -dx, +y direction
+                    dir_idx_t di = dirToDirIdx(-node->dx, 1);
+                    grid->getCellKeyAndCoord(di, vert_coord, vert_key, kc_min, pc_min);
+                    _dbg11("[Cone:-Y] 1. Ray cuts thru cur row -y vertex and ray.y < 0. First cell is (" << pc_min << "), obtained from next row (floored) vertex(" << vert_coord << ")");
+                }
+                else
+                { // kc_min depends on current cell
+                    const V2 vert_coord = node->crn->coord + node->v_min;
+                    const mapkey_t vert_key = grid->coordToKey<false>(vert_coord);
+                    dir_idx_t di = dirToDirIdx(node->dx, 1);
+                    grid->getCellKeyAndCoord(di, vert_coord, vert_key, kc_min, pc_min);
+                    _dbg11("[Cone:-Y] 2. Ray cuts thru cur row -y vertex and ray.y >= 0. First cell is (" << pc_min << "), obtained from cur row -y vertex(" << vert_coord << ")");
+                }
+            }
+            else
+            { // ray cuts across edge in +y direction after min-y (floored) vertex on expanded interval (cur row)
+                if (node->v_min.y < 0)
+                { // kc_min depends on next row
+                    int_t dx_next = node->dx + (node->dx < 0 ? -1 : 1);
+                    const V2 vert_coord = node->crn->coord + V2{dx_next, dx_next * node->v_min.y / node->v_min.x};
+                    const mapkey_t vert_key = grid->coordToKey<false>(vert_coord);
+                    // kc_min is in -dx, +y direction
+                    dir_idx_t di = dirToDirIdx(-node->dx, 1);
+                    grid->getCellKeyAndCoord(di, vert_coord, vert_key, kc_min, pc_min);
+                    _dbg11("[Cone:-Y] 3. Ray cuts thru edge and ray.y < 0. First cell is (" << pc_min << "), obtained from next row (floored) vertex(" << vert_coord << ")");
+                }
+                else
+                { // kc-min depends on the current floored vertex
+                    const V2 vert_coord = node->crn->coord + V2{node->dx, node->dx * node->v_min.y / node->v_min.x};
+                    const mapkey_t vert_key = grid->coordToKey<false>(vert_coord);
+                    // kc_min is in +dx, +y direction
+                    dir_idx_t di = dirToDirIdx(node->dx, 1);
+                    grid->getCellKeyAndCoord(di, vert_coord, vert_key, kc_min, pc_min);
+                    _dbg11("[Cone:-Y] 4. Ray cuts thru edge and ray.y < 0. First cell is (" << pc_min << "), obtained from cur row (floored) vertex(" << vert_coord << ")");
+
+                }
+                const V2 vert_coord = node->crn->coord + V2{node->dx, node->dx * node->v_min.y / node->v_min.x};
+                const mapkey_t vert_key = grid->coordToKey<false>(vert_coord);
             }
 
             _dbgdec;
